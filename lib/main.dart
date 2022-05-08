36fe1779a9bc592300eebfe9891ecf85ccd9808e
Heiko Schmid <heikoschmid10@gmail.com>
2022-05-08 17:24:23 +0200

    initial homepage

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:crypto_tracker/widgets/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Stocks", home: HomePage());
+    return const MaterialApp(title: "Stocks", home: HomePage());
   }
 }
