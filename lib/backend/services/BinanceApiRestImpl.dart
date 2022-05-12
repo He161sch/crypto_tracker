2eafccc4a4146b655506a85475f9133d45c49d83
furkankarayel <furkarayel@hotmail.com>
2022-05-12 12:08:40 +0200

    Riverpod, NavigationBar & Binance REST added

--- lib/backend/services/BinanceApiRestImpl.dart
+++ lib/backend/services/BinanceApiRestImpl.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
-import 'package:myapp/backend/services/BinanceApiRest.dart';
-import 'package:myapp/backend/ui-kit/currency_model.dart';
+import 'package:crypto_tracker/backend/services/BinanceApiRest.dart';
+import 'package:crypto_tracker/backend/ui-kit/currency_model.dart';
 
 class BinanceApiRestImpl extends ApiServiceAggregator {
   @override
   Future<List<CurrencyModel>> loadCurrencies() async {
     Response response =
-    await get(Uri.parse('https://api.binance.com/api/v3/ticker/price'));
+        await get(Uri.parse('https://api.binance.com/api/v3/ticker/price'));
     if (response.statusCode == 200) {
       Iterable data = json.decode(response.body);
       await Future.delayed(const Duration(milliseconds: 5000));
