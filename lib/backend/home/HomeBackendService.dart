c340b9b64eba43bbe0c1d8c762d3cbf478799a70
furkankarayel <furkarayel@hotmail.com>
2022-05-27 16:32:48 +0200

    Backend Fetch works, Navigation and Page structure

--- lib/backend/home/HomeBackendService.dart
+++ lib/backend/home/HomeBackendService.dart
@@ -2,4 +2,6 @@ import 'package:crypto_tracker/backend/ui-kit/currency_model.dart';
 
 abstract class HomeBackendService {
   Future<List<CurrencyModel>> loadCurrencies();
+
+  getAllCurrencies() {}
 }
